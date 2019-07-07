caadd2cfecc1812b28e3b56e6194282b06c70533
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-07-07 19:47:43 +0900

    feat(test): add tests

---
+++ __tests__/trigger.test.ts
@@ -0,0 +1,167 @@
+import * as trigger from "../src/index";
+
+function setup() {
+  process.env.ACK_EXCHANGE = "coinz";
+  process.env.ACK_ORDER_AMOUNT = "1000";
+  process.env.ACK_ORDER_TICKERS = "BTC,ETH,XRP";
+  process.env.ACK_COINZ_API_KEY = "*****";
+  process.env.ACK_COINZ_API_SECRET = "*****";
+}
+
+function cleanup() {
+  delete process.env.ACK_EXCHANGE;
+  delete process.env.ACK_ORDER_AMOUNT;
+  delete process.env.ACK_ORDER_TICKERS;
+  delete process.env.ACK_COINZ_API_KEY;
+  delete process.env.ACK_COINZ_API_SECRET;
+}
+
+describe("trigger#", () => {
+  describe("fetchExchange", () => {
+    it("returns IExchange", () => {
+      setup();
+
+      const exchange = trigger.fetchExchange();
+      expect(exchange).toBeInstanceOf(Object);
+    });
+  });
+
+  describe("verifyVariables", () => {
+    let isVerified: boolean;
+    describe("all variables are setted", () => {
+      beforeAll(() => {
+        setup();
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns true", () => {
+        expect(isVerified).toBeTruthy();
+      });
+    });
+
+    describe("ACK_EXCHANGE is not set", () => {
+      beforeAll(() => {
+        setup();
+        delete process.env.ACK_EXCHANGE;
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+
+    describe("ACK_EXCHANGE is invalid value", () => {
+      beforeAll(() => {
+        setup();
+        process.env.ACK_EXCHANGE = "bitflyer";
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+
+    describe("ACK_ORDER_AMOUNT is not set", () => {
+      beforeAll(() => {
+        setup();
+        delete process.env.ACK_ORDER_AMOUNT;
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+
+    describe("ACK_ORDER_AMOUNT is not a number", () => {
+      beforeAll(() => {
+        setup();
+        process.env.ACK_ORDER_AMOUNT = "123hello";
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+
+    describe("ACK_ORDER_TICKERS is not set", () => {
+      beforeAll(() => {
+        setup();
+        delete process.env.ACK_ORDER_TICKERS;
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+
+    describe("ACK_ORDER_TICKERS includes invalid ticker", () => {
+      beforeAll(() => {
+        setup();
+        process.env.ACK_ORDER_TICKERS = "BTC,ETH,XRP,ZEC";
+
+        isVerified = trigger.verifyVariables();
+      });
+
+      it("returns false", () => {
+        expect(isVerified).toBeFalsy();
+      });
+    });
+  });
+
+  describe("calcOrder", () => {
+    let order: number;
+
+    describe("amount: 1000, ask: 1221779, limit: {min: 0.0001, max: 25}", () => {
+      beforeAll(() => {
+        order = trigger.calcOrder(1000, 1221779, { min: 0.0001, max: 25 });
+      });
+
+      it("returns 0.0008", () => {
+        expect(order).toBe(0.0008);
+      });
+    });
+
+    describe("amount: 1000, ask: 31434, limit: {min: 0.01, max: 300}", () => {
+      beforeAll(() => {
+        order = trigger.calcOrder(1000, 31434, { min: 0.01, max: 300 });
+      });
+
+      it("returns 0.03", () => {
+        expect(order).toBe(0.03);
+      });
+    });
+
+    describe("amont: 1000, ask: 12836, limit: {min: 0.1, max: 500}", () => {
+      beforeAll(() => {
+        order = trigger.calcOrder(1000, 12836, { min: 0.1, max: 500 });
+      });
+
+      it("returns 0", () => {
+        expect(order).toBe(0);
+      });
+    });
+
+    describe("amount: 10000000, ask: 42.948, limit: { min: 10, max: 100}", () => {
+      beforeAll(() => {
+        order = trigger.calcOrder(100000, 42.948, { min: 10, max: 100 });
+      });
+
+      it("returns 100", () => {
+        expect(order).toBe(100);
+      });
+    });
+  });
+
+  afterEach(() => cleanup());
+});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { AzureFunction, Context } from "@azure/functions";
 
 import { EXCHANGES } from "./exchanges";
-import { IExchange } from "./exchanges/exchange";
+import { IExchange, Limit } from "./exchanges/exchange";
 import { tryParseInt } from "./utils";
 
-function fetchExchange(): IExchange {
+export function fetchExchange(): IExchange {
   return EXCHANGES.filter(w => w.id === process.env.ACK_EXCHANGE)[0];
 }
 
-function verifyVariables(): boolean {
+export function verifyVariables(): boolean {
   let success = true;
 
   // ACK_EXCHANGE is required
@@ -28,12 +28,18 @@ function verifyVariables(): boolean {
   return success;
 }
 
-async function notify(message: string): Promise<void> {
+export async function notify(message: string): Promise<void> {
   console.log(message);
   // TODO: Impl
 }
 
-async function run(context: Context, timer: any): Promise<void> {
+export function calcOrder(amount: number, ask: number, limit: Limit): number {
+  const digit = 1 / limit.min;
+  const order = Math.floor((amount / ask) * digit) / digit;
+  return order >= limit.max ? limit.max : order;
+}
+
+export async function run(context: Context, timer: any): Promise<void> {
   const isVerified = verifyVariables();
   if (!isVerified) {
     console.error("Environment variables checks failed. Please check your configurations");
@@ -56,15 +62,14 @@ async function run(context: Context, timer: any): Promise<void> {
   const tickers = await exchange.fetchTickers();
   for (let symbol of symbols) {
     const limit = exchange.limits[symbol];
-    const digit = 1 / limit.min;
-    const order = Math.floor((amounts / tickers[symbol].ask) * digit) / digit;
+    const order = calcOrder(amounts, tickers[symbol].ask, exchange.limits[symbol]);
     if (order < limit.min) {
       await notify(`SKIPPED: Could not place an order with the current ${symbol} price: ${tickers[symbol].ask}.`);
       continue;
     }
 
-    await exchange.makeBuyMarketOrder(symbol, order >= limit.max ? limit.max : order);
-    await notify(`ORDERD: Placed a marked order for ${symbol} at ${order >= limit.max ? limit.max : order}${symbol}.`);
+    await exchange.makeBuyMarketOrder(symbol, order);
+    await notify(`ORDERD: Placed a marked order for ${symbol} at ${order}${symbol}.`);
   }
 }
 
